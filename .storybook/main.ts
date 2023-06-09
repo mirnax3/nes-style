e44ecb734aa3025d72b454cde9317c60f82ed5ad
Mirna Plakalović <79122766+mirnaio@users.noreply.github.com>
2023-06-09 01:40:43 -0400

    update storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,17 +1,13 @@
 import type { StorybookConfig } from "@storybook/react-webpack5";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
   framework: {
     name: "@storybook/react-webpack5",
-    options: {},
+    options: {}
   },
   docs: {
-    autodocs: "tag",
-  },
+    autodocs: true
+  }
 };
 export default config;
