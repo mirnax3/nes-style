0a78a1e64bb5668e5b0165d5911bfcb3d10b73f0
Mirna Plakalović <79122766+mirnaio@users.noreply.github.com>
2023-06-10 03:15:29 -0400

    remove index

--- src/components/Typography/index.ts
+++
@@ -1,6 +0,0 @@
-export { HeadingText } from './HeadingText';
-export { BodyText } from './BodyText';
-export { MetaText } from './MetaText';
-export { SmallText } from './SmallText';
-export * from './styles';
-export * from './props';
