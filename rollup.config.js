c3879108f9c792f294ee6f505453a6eb6e808caa
Mirna Plakalović <79122766+mirnaio@users.noreply.github.com>
2023-10-09 22:34:46 -0400

    fix global types declaration

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
-import typescript from 'rollup-plugin-typescript2';\
+import typescript from 'rollup-plugin-typescript2';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const packageJson = require('./package.json');
 
--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -8,4 +8,11 @@ declare global {
   type ParentClassNameProp = {
     className?: string;
   };
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  type GenericHandler = (e?: any) => void;
+
+  type ClickEventProp = {
+    onClick?: GenericHandler;
+  };
 }
